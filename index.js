2f759eb601633475d24977f0221f217c9feb47f3
Ledu996 <dusan.dorcol@hotmail.com>
2023-11-02 11:50:01 +0100

    Deleted comments and console logs in index

--- index.js
+++ index.js
@@ -20,47 +20,35 @@ app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// what confuses me is the flow, and also is driver a user of a system 
-// or just and entity that admin controls 
-// if driver is type user, that properties inside of that entity has to be the same as users
-// only the role is different inside user
-// maybe has to do something with that create user entity (parent class)
-// but have customer, driver and admin (children classes)
-
-// exclude a param from a middleware function not a query 
-// here the first argument is the path
-// create a dynamic route handler here 
 
 
 app.use(async (req, res, next) => {
   try {
-  // did not solve a problem because maybe id will be required in whiteListed route
-  // find out how to get id param
-  const { originalUrl } = req;
-  // unprotected routes 
   
+  const { originalUrl } = req;
+
+  // unprotected routes 
   const paths = [
-    /\/users\/signup/,  // Regular expression to match the string "users/signup"
-    /\/users\/signin/,  // Regular expression to match the string "users/signin"
-    /\/products\/all/, // Regular expression to match the string "products/all"
-    /\/products\/id\/\w+/  // Regular expression to match routes with "id" parameter
+    /\/users\/signup/,  
+    /\/users\/signin/,  
+    /\/products\/all/, 
+    /\/products\/id\/\w+/  
   ]
   
-    const isWhiteListed = paths.some(e => e.test(originalUrl)); // returns true based on condition
+  const isWhiteListed = paths.some(e => e.test(originalUrl)); 
     
-    if (!isWhiteListed) {
-      const token = req.headers.authorization.split(' ')[1];
-      const verified = await jwt.verify(token, 'secret');
-      if (!verified) return res.status(403).json({message: 'User is not authorized'});
-      req.user = { _id: verified._id }
-      console.log('Log request in user', req.user);
-      next()
-    } else {
-      console.log('Not a protected route');
-      next(); // not among protected paths
-    }
+  if (!isWhiteListed) {
+        
+    const token = req.headers.authorization.split(' ')[1];
+    const verified = await jwt.verify(token, 'secret');
+        
+    if (!verified) return res.status(403).json({message: 'User is not authorized'});
+        req.user = { _id: verified._id }
+        next()
+      } else {
+          next(); // not among protected paths
+      }
 } catch (err) {
-  console.log(err);
   return res.status(403).json({message: 'User has to be authorized'});
 } 
 })
@@ -72,8 +60,7 @@ app.use('/users', userRouter);
 app.use('/orders', orderRouter);
 app.use('/products', productRouter);
 app.use('/drivers', driverRouter);
-// notification 
-// connect our app to aws, or firebase(file uploads, and notifications)
+
 
 mongoose.connect('mongodb+srv://dusan_admin2:admin22@baza.avgt5.mongodb.net/product_order');
   
