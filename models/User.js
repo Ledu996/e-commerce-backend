30f001c54e032b4672e24f5b47ec2d51aeab7e42
Ledu996 <dusan.dorcol@hotmail.com>
2023-11-08 23:11:02 +0100

    updated User and Driver models

--- models/User.js
+++ models/User.js
@@ -13,6 +13,8 @@ const options = {
 // export the address schema to UserSchema
 // address as a separate entity
 const UserSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { 
         type: String, 
@@ -23,13 +25,13 @@ const UserSchema = new Schema({
         match: [ validEmail, 'Please enter a valid address' ] 
     },
     
-    password: { type: String, required: true }, // was se to select false
+    password: { type: String, required: true }, 
     isActive: { type: Boolean, default: false },
     createdAt:  {type: Date, default: new Date() },
     dateOfBirth: Date,
     gender: { type: String, enum: genders, required: true },
     role: { type: String, enum: roles },
-    addresses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'address' }],
+    address: { type: mongoose.Schema.Types.ObjectId, ref: 'address' },
     defaultAddress: { type: mongoose.Schema.Types.ObjectId, ref: 'address' },  
     verificationToken: String,
     isVerified: { type: Boolean, default: false } 
